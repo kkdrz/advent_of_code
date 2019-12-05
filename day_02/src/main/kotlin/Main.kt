0ebe944d35128cfcd3d5ed265ed5aa2225c5d100
Konrad Drozd <drozkonrad@gmail.com>
2019-12-06 00:35:06 +0100

    Day 2 part 2 solution

--- day_02/src/main/kotlin/Main.kt
+++ day_02/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 fun main() {
     val computer = Computer()
+    part1(computer);
+    part2(computer);
+
+
+}
+
+fun part1(computer: Computer) {
 
     val program = computer.readProgramFromFile("input.txt").toMutableList();
 
@@ -7,4 +14,24 @@ fun main() {
     program[2] = 2
 
     println(computer.compute(program)[0])
+}
+
+fun part2(computer: Computer) {
+
+    val program = computer.readProgramFromFile("input.txt");
+
+    for (noun in 0 until 99) {
+        for (verb in 0 until 99) {
+            val mutableProgram = program.toMutableList();
+            mutableProgram[1] = noun
+            mutableProgram[2] = verb
+            if (computer.compute(mutableProgram)[0] == 19690720) {
+                println("noun = $noun")
+                println("verb = $verb")
+                println("answer = ${100 * noun + verb}")
+                return;
+            }
+        }
+    }
+
 }
