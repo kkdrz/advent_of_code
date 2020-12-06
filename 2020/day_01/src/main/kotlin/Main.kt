22815190cbf253c9d228d466ed441b155a981dca
Konrad Drozd <drozdkonrad@gmail.com>
2020-12-06 21:00:05 +0100

    [Day 01 part 2] Solution

--- 2020/day_01/src/main/kotlin/Main.kt
+++ 2020/day_01/src/main/kotlin/Main.kt
@@ -4,11 +4,14 @@ fun main() {
 
     val expenses = File("src/main/resources/input.txt").readLines().map { it.toInt() }
 
+
     expenses.forEach { i ->
         expenses.forEach { j ->
-            if (i + j == 2020) {
-                println(i * j)
-                return
+            expenses.forEach { k ->
+                if (i + j + k == 2020) {
+                    println(i * j * k)
+                    return
+                }
             }
         }
     }
