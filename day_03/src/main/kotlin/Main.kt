9c2843d9d89f2b33c0b663e76abe28e9e8eeb281
Konrad Drozd <drozdkonrad@gmail.com>
2020-12-06 22:53:56 +0100

    [Day 03 part 2] Solution

--- day_03/src/main/kotlin/Main.kt
+++ day_03/src/main/kotlin/Main.kt
@@ -5,11 +5,21 @@ fun main() {
     val map = File("src/main/resources/input.txt")
         .readLines().toMutableList()
 
+    val result = arrayOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
+        .map { getNumberOfTrees(mutableListOf<String>().apply { addAll(map) }, it.second, it.first) }
+        .map { it.toLong() }
+        .reduce { acc, n -> acc * n }
+
+    println(result)
+}
+
+private fun getNumberOfTrees(
+    map: MutableList<String>,
+    slopeDown: Int,
+    slopeRight: Int
+): Int {
     val maxY = map.size - 1
     val maxX = map[0].length - 1
-
-    val slopeRight = 3
-    val slopeDown = 1
 
     var x = 0
     var y = 0
@@ -27,5 +37,5 @@ fun main() {
 
     }
 
-    println(trees)
+    return trees
 }
