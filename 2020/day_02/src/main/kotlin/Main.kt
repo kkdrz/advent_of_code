9ca17d51d472a8cc00eae3b39a26e81311a8c801
Konrad Drozd <drozdkonrad@gmail.com>
2020-12-06 22:04:45 +0100

    [Day 02 part 2] Solution

--- 2020/day_02/src/main/kotlin/Main.kt
+++ 2020/day_02/src/main/kotlin/Main.kt
@@ -16,17 +16,30 @@ private fun decodePassword(encoded: String): Password {
     val max = encoded.substring(encoded.indexOf('-') + 1, encoded.indexOf(' ')).toInt()
     val char = encoded.substring(encoded.indexOf(' ') + 1, encoded.indexOf(':')).first()
     val password = encoded.substring(encoded.indexOf(": ") + 2)
-    return Password(password, OneLetterPolicy(min, max, char))
+    return Password(password, ExactlyOnePositionPolicy(min, max, char))
 }
 
-class Password(val text: String, val policy: OneLetterPolicy) {
+
+class Password(val text: String, val policy: Policy) {
 
     fun isValid(): Boolean = policy.isValid(text)
 }
 
-class OneLetterPolicy(val min: Int, val max: Int, val char: Char) {
+interface Policy {
+    fun isValid(password: String): Boolean
+}
 
-    fun isValid(password: String): Boolean {
-        return password.toCharArray().filter(predicate = { it == char }).count() in min..max
+class OneLetterPolicy(val min: Int, val max: Int, val char: Char) : Policy {
+
+    override fun isValid(password: String): Boolean {
+        return password.toCharArray().filter { it == char }.count() in min..max
     }
+}
+
+class ExactlyOnePositionPolicy(val first: Int, val second: Int, val char: Char) : Policy {
+
+    override fun isValid(password: String): Boolean {
+        return (password[first - 1] == char).xor(password[second - 1] == char)
+    }
+
 }
