faf92a7da5ec94d516b2e153ec5837c316e1a0ba
Konrad Drozd <drozdkonrad@gmail.com>
2020-12-12 20:48:59 +0100

    [Day 12 part 2] Solution

--- 2020/day_12/src/main/kotlin/Main.kt
+++ 2020/day_12/src/main/kotlin/Main.kt
@@ -6,8 +6,17 @@ fun main() {
             .readLines()
             .map { it[0] to it.substring(1).toInt() }
 
-    val ship = Ship()
+    val ship1 = Part1Ship()
+    execShip(data, ship1)
+    println(abs(ship1.x) + abs(ship1.y))
 
+    val ship2 = Part2Ship()
+    execShip(data, ship2)
+    println(abs(ship2.x) + abs(ship2.y))
+
+}
+
+private fun execShip(data: List<Pair<Char, Int>>, ship: Ship) {
     data.forEach { command ->
         when (command.first) {
             'N' -> ship.goNorth(command.second)
@@ -19,34 +28,83 @@ fun main() {
             'F' -> ship.goForward(command.second)
         }
     }
-
-    println(abs(ship.x) + abs(ship.y))
 }
 
-class Ship {
+interface Ship {
+    fun goNorth(units: Int)
+    fun goSouth(units: Int)
+    fun goEast(units: Int)
+    fun goWest(units: Int)
+    fun goForward(units: Int)
+    fun rotateLeft(degrees: Int)
+    fun rotateRight(degrees: Int)
+}
+
+class Part2Ship : Ship {
+
+    var x = 0
+    var y = 0
+
+    var waypointX = 10
+    var waypointY = -1
+
+    override fun goNorth(units: Int) {
+        waypointY -= units
+    }
+
+    override fun goSouth(units: Int) {
+        waypointY += units
+    }
+
+    override fun goEast(units: Int) {
+        waypointX += units
+    }
+
+    override fun goWest(units: Int) {
+        waypointX -= units
+    }
+
+    override fun goForward(units: Int) {
+        x += units * waypointX
+        y += units * waypointY
+    }
+
+    override fun rotateLeft(degrees: Int) {
+        for (i in 0 until degrees / 90) {
+            waypointY = -waypointX.also { waypointX = waypointY }
+        }
+    }
+
+    override fun rotateRight(degrees: Int) {
+        rotateLeft(360 - degrees)
+    }
+
+}
+
+open class Part1Ship : Ship {
     var x = 0
     var y = 0
 
     var direction: Int = 0
     val directions: List<Char> = listOf('E', 'N', 'W', 'S')
 
-    fun goNorth(units: Int) {
+    override fun goNorth(units: Int) {
         y -= units
     }
 
-    fun goSouth(units: Int) {
+    override fun goSouth(units: Int) {
         y += units
     }
 
-    fun goEast(units: Int) {
+    override fun goEast(units: Int) {
         x += units
     }
 
-    fun goWest(units: Int) {
+    override fun goWest(units: Int) {
         x -= units
     }
 
-    fun goForward(units: Int) {
+    override fun goForward(units: Int) {
         when (directions[direction]) {
             'E' -> x += units
             'W' -> x -= units
@@ -55,11 +113,11 @@ class Ship {
         }
     }
 
-    fun rotateLeft(degrees: Int) {
+    override fun rotateLeft(degrees: Int) {
         direction = (direction + (degrees / 90)) % directions.size
     }
 
-    fun rotateRight(degrees: Int) {
+    override fun rotateRight(degrees: Int) {
         rotateLeft(360 - degrees)
     }
 
