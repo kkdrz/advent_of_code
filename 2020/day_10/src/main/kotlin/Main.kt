68958141200e56c520afe97182892c1a7a44ce0c
Konrad Drozd <drozdkonrad@gmail.com>
2020-12-12 11:40:01 +0100

    [Day 10 Part 2] Solution

--- 2020/day_10/src/main/kotlin/Main.kt
+++ 2020/day_10/src/main/kotlin/Main.kt
@@ -1,8 +1,25 @@
 import java.io.File
+import java.util.function.BiFunction
 
 fun main() {
     val data = File("src/main/resources/input.txt").readLines().map { it.toInt() }.toMutableList()
+    data.add(0)
 
+    part1(data.toMutableList())
+
+    val count = mutableMapOf<Int, Long>(0 to 1)
+
+    data.sorted().forEach {
+        for (i in 1..3) {
+            count.merge(it + i, count[it]!!) { t, u -> t + u }
+        }
+    }
+
+    println("Part 2: ${count[count.keys.maxOrNull()!!]}")
+}
+
+
+private fun part1(data: MutableList<Int>) {
     val chain = mutableListOf<Int>()
     val differences = mutableMapOf<Int, Int>()
     var currentAdapter = 0
@@ -16,7 +33,6 @@ fun main() {
     }
     differences[3] = differences[3]!! + 1 //built-in adapter
     println("Part 1: ${differences[1]!! * differences[3]!!}")
-
 }
 
 fun findWeakestAdapterForInputOf(jolts: Int, data: MutableList<Int>): Int {
